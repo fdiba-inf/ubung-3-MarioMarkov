b6cf9f8e00fee99a2e200db708c8b9922362eb72
MarioMarkov <mario11septemvri@gmail.com>
2020-10-13 20:59:29 +0000

    fiki

--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -16,17 +16,19 @@ public class Triangle {
           double c = input.nextDouble();
           p =(a+b+c)/2;
           if (a<=0||b<=0||c<=0){
-            break;
+            break; 
           }
           if (a>0 && b>0&& c>0 && a+b>c && a+c>b&& b+c>a ){
             area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
             System.out.println("Area: "+ area);
-            if(a==b||a==c||c==b){
-              System.out.println("Triangle: isosceles");
+            if(a==b && b==c)
+            System.out.println("Equilateral");
 
-            }else if(a==b||a==c||c==b){
+        else if ((a==b && b!=c ) || (a!=b && c==a) || (c==b && c!=a))
+            System.out.println("Isosceles");
 
-            }
+        else if(a!=b && b!=c && c!=a)
+            System.out.println("Scalene");
           } 
           else{
             System.out.println("Values are not correct!");
