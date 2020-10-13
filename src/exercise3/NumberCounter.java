bb4b53e8dd8934978dde374cada235c2cbdc98c9
MarioMarkov <mario11septemvri@gmail.com>
2020-10-13 15:14:43 +0000

    1

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,36 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NumberCounter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int positive=0;
+        int negative=0;
+        int sum=0;
+        int average=0;
+        int countofNumbers=0;
+        while(true){
+          int num = input.nextInt();
+          if(num ==0){
+              break;
+          } 
+          if(num >0){
+            positive++;
+          }
+          else{
+            negative++;
+          }
+          sum+=num;
+          countofNumbers++;
+        }
+        average = sum /countofNumbers;
+        System.out.println("Positive numbers: "+ positive);
+        System.out.println("Negative numbers: " + negative);
+        System.out.println("Sum: "+ sum);
+        System.out.println("Average: "+ average);
+
+    }
+
+}
